795a550acee83ff3ff63bb719bd28152e797dfe3
olawale olaleye <waleberry@yahoo.com>
2020-05-21 18:19:05 +0100

    declared region

--- run-ec2-instance.sh
+++ run-ec2-instance.sh
@@ -4,6 +4,7 @@
 INSTANCE_ID=$(aws ec2 describe-instances \
     --filter Name=tag:Name,Values=$INSTANCE_NAME \
     --query 'Reservations[*].Instances[*].{Instance:InstanceId,Status:State.Name,Name:Tags[?Key==`Name`]|[0].Value}' \
+    --region $REGION \
     --output text | grep 'running' | awk -F' ' '{print $1}')
 
 aws ssm start-session --target $INSTANCE_ID --region $REGION
