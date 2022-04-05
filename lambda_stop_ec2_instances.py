afe2e39870888f1569b4c26ed194d087704f935c
Olawale Olaleye <waleberry@yahoo.com>
2022-04-05 16:30:40 +0100

    Update lambda_stop_ec2_instances.py
    defined the lambda_handler function

--- lambda_stop_ec2_instances.py
+++ lambda_stop_ec2_instances.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import time
 
 # deploy to AWS Lambda
 
@@ -10,43 +11,48 @@ instances = ['i-xxxxxxxxx',
 
 ec2 = boto3.client('ec2', region_name=region)
 
-
-def stop_instances():
-    response = ec2.stop_instances(
-        InstanceIds=instances,
-        Force=True
-    )
-    print('stopped your instances: ' + str(instances))
-
-
-def get_instances():
-
-    reservations = ec2.describe_instances(Filters=[
-        {
-            "Name": "instance-state-name",
-            "Values": ["running", "stopped"],
-        }
-    ]).get("Reservations")
-
-    for reservation in reservations:
-        for instance in reservation["Instances"]:
-            instance_id = instance["InstanceId"]
-            instance_type = instance["InstanceType"]
-            private_ip = instance["PrivateIpAddress"]
-            instance_state = instance["State"]["Name"]
-            instance_name = instance["Tags"][0]["Value"]
-            print(f"{instance_id} {instance_type} {private_ip} {instance_state} {instance_name}")
+def lambda_handler(event, context):
+            def stop_instances():
+                response = ec2.stop_instances(
+                    InstanceIds=instances,
+                    Force=True
+                )
+                print('stopped your instances: ' + str(instances))
 
 
-print('==========================================Describing Instances before '
-      'stopping=============================================')
+            def get_instances():
 
-get_instances()
+                reservations = ec2.describe_instances(Filters=[
+                    {
+                        "Name": "instance-state-name",
+                        "Values": ["running", "stopped"],
+                    }
+                ]).get("Reservations")
 
-print('==========================================Stopping Some Instances=============================================')
+                for reservation in reservations:
+                    for instance in reservation["Instances"]:
+                        instance_id = instance["InstanceId"]
+                        instance_type = instance["InstanceType"]
+                        private_ip = instance["PrivateIpAddress"]
+                        instance_state = instance["State"]["Name"]
+                        instance_name = instance["Tags"][0]["Value"]
+                        print(f"{instance_id} {instance_type} {private_ip} {instance_state} {instance_name}")
 
-stop_instances()
 
-print('==========================================Describing Instances=============================================')
+            print ("Start : %s" % time.ctime())
 
-get_instances()
+            print('==========================================Describing Instances before '
+                  'stopping=============================================\n')
+
+            get_instances()
+
+            print('==========================================Stopping Some Instances=============================================\n')
+
+            stop_instances()
+
+            time.sleep( 5 )
+            print ("End : %s" % time.ctime())
+
+            print('==========================================Describing Instances=============================================')
+
+            get_instances()
